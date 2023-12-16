002d5ace610b75e03fda9f2794f9252fe4244161
Ryan Caldwell <ryanjcaldwell4@gmail.com>
2023-12-16 15:24:14 -0500

    Horribly inefficient Day 5 Part 1

--- 5/main.go
+++ 5/main.go
@@ -28,6 +28,15 @@ type Mapper struct {
 	Ranges   []Range
 }
 
+func (m *Mapper) GetDestinationNumber(sourceNumber int) int {
+	for _, r := range m.Ranges {
+		if sourceNumber >= r.SourceRangeStart && sourceNumber <= r.SourceRangeStart+(r.RangeLength-1) { // e.g. 90, 2 is 90 and 91, not 90, 91, 92
+			return r.DestinationRangeStart + (sourceNumber - r.SourceRangeStart)
+		}
+	}
+	return sourceNumber
+}
+
 func (m *Mapper) String() string {
 	return fmt.Sprintf("Mapper: %s\nFromType: %s\nToType: %s\nRanges: %v\n", m.Name, m.FromType, m.ToType, m.Ranges)
 }
@@ -38,10 +47,11 @@ type Range struct {
 	RangeLength           int
 }
 
-func getMappers(lines []string) []Mapper {
-	mappers := []Mapper{}
+func getOrderedMappers(lines []string) map[int]*Mapper {
+	orderedMappers := map[int]*Mapper{}
 
 	lineIdx := 2
+	mapperCount := 0
 	for lineIdx != len(lines) {
 		line := lines[lineIdx]
 		// if we hit a blank line, we've reached the end of a mapper definition
@@ -75,18 +85,43 @@ func getMappers(lines []string) []Mapper {
 				lineIdx++
 			}
 		}
-		mappers = append(mappers, mapper)
+		orderedMappers[mapperCount] = &mapper
+		mapperCount++
 	}
-	return mappers
+	return orderedMappers
+}
+
+func traverseMappersForSeedLocations(seeds []int, mappers map[int]*Mapper) []int {
+	locationNumbers := []int{}
+
+	for _, seed := range seeds {
+		currentValue := seed
+		// iterate through each mapper in order and update currentValue
+		for i := 0; i < len(mappers); i++ {
+			currentValue = mappers[i].GetDestinationNumber(currentValue)
+		}
+		locationNumbers = append(locationNumbers, currentValue)
+	}
+
+	return locationNumbers
+}
+
+func getMinValue(numbers []int) int {
+	min := numbers[0]
+	for _, n := range numbers {
+		if n < min {
+			min = n
+		}
+	}
+	return min
 }
 
 func main() {
 	lines := fileReader.GetLines()
 	seeds := getSeeds(lines)
-	mappers := getMappers(lines)
+	mappers := getOrderedMappers(lines)
 
-	fmt.Println("Seeds: ", seeds)
-	for _, m := range mappers {
-		fmt.Println(m.String())
-	}
+	// Part 1
+	results := traverseMappersForSeedLocations(seeds, mappers)
+	fmt.Println("Part 1: ", getMinValue(results))
 }
