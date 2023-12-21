48651e4ff755e373603fab0d88fea6cc09778b6d
Ryan Caldwell <ryanjamescaldwell@github.com>
2023-12-21 16:37:36 -0500

    Add some notes

--- 9/main.go
+++ 9/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/ryanjamescaldwell/adventofcode2023/fileReader"
 	"github.com/ryanjamescaldwell/adventofcode2023/helpers"
@@ -23,5 +24,13 @@ func main() {
 		fmt.Println(input[i])
 	}
 
+	if os.Args[1] == "sample.txt" {
+		fmt.Println("Sample answer: 114")
+	}
+
+	// TODO
+	// For each history (line), we need to figure out the next number and add it to an array
+	// We shouldn't need to wait for 0, just need all numbers in the line to be the same
+	// Once each history's next number is identified, sum them.
 	fmt.Println("Part 1: ", 0)
 }
